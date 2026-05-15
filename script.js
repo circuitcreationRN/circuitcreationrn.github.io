b4b1f5ea32101855378861cc4c11798aac0e830f
circuitcreationRN <circuitcreation0@gmail.com>
2026-05-15 21:06:03 +0530

    Add cursor glow effect on mouse movement

--- script.js
+++ script.js
@@ -36,3 +36,13 @@ function typing(){
 }
 
 typing();
+
+const glow = document.querySelector(".cursor-glow");
+
+document.addEventListener("mousemove",(e)=>{
+
+    glow.style.left = e.pageX + "px";
+
+    glow.style.top = e.pageY + "px";
+
+});
