231830382cb8fa4a9b4533b8755d7cad6214da79
circuitcreationRN <circuitcreation0@gmail.com>
2026-05-19 15:32:46 +0530

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,1 +1,25 @@
 
+// LOADER SYSTEM
+
+window.addEventListener("load", function(){
+
+    const loader =
+    document.getElementById("loader");
+
+    if(loader){
+
+        setTimeout(function(){
+
+            loader.style.opacity = "0";
+
+            setTimeout(function(){
+
+                loader.style.display = "none";
+
+            },500);
+
+        },500);
+
+    }
+
+});
